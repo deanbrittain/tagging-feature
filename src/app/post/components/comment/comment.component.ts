090c974e3fc4851c7c9738a5956b8735d2470687
Dean Brittain <deanbrittain@Deans-MBP.sumo.io>
2023-08-23 20:05:13 -0600

    add @ detection

--- src/app/post/components/comment/comment.component.ts
+++ src/app/post/components/comment/comment.component.ts
@@ -11,6 +11,13 @@ export class CommentComponent {
   showCommentInput = false;
   newCommentText = '';
   comments: Comment[] = [];
+  users = [
+    { userID: 1, name: 'Kevin' },
+    { userID: 2, name: 'Jeff' },
+    { userID: 3, name: 'Bryan' },
+    { userID: 4, name: 'Gabbey' },
+  ];
+  filteredUsers: any[] = [];
 
   @Output() commentIconClicked = new EventEmitter<void>();
 
@@ -29,5 +36,28 @@ export class CommentComponent {
       this.comments.push(newComment);
       this.newCommentText = '';
     }
+  }
+  // Variable to hold the current input value
+  commentInputValue: string = '';
+
+  // Method to detect '@' symbol
+  onCommentInputChange(event: any) {
+    this.commentInputValue = event.target.value;
+    this.newCommentText = this.commentInputValue; // Update the new comment text
+
+    if (this.commentInputValue.includes('@')) {
+      // Logic to handle '@' symbol detection
+      console.log('Detected @ symbol');
+    }
+  }
+
+  selectUser(user: any) {
+    const atIndex = this.commentInputValue.lastIndexOf('@');
+    const beforeAt = this.commentInputValue.slice(0, atIndex);
+    const afterAt = this.commentInputValue.slice(atIndex).split(' ')[1] || '';
+
+    this.commentInputValue = `${beforeAt}@${user.name} ${afterAt}`;
+    this.newCommentText = this.commentInputValue; // Update the new comment text
+    this.filteredUsers = []; // Clear the filtered users
   }
 }
