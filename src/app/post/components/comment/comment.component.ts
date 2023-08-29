febac8f8335b4b65eb73302f886e91d8a6948d37
Dean Brittain <deanbrittain@Deans-MacBook-Pro.local>
2023-08-28 19:16:53 -0600

    fix comment input not clearing

--- src/app/post/components/comment/comment.component.ts
+++ src/app/post/components/comment/comment.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { Comment } from '../../models/comment.model';
+import { CommentModel } from '../../models/comment.model';
 
 @Component({
   selector: 'app-comment',
@@ -7,46 +7,61 @@ import { Comment } from '../../models/comment.model';
   styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent {
-  @Input() comment!: Comment;
+  // Input property to receive the comment data from the parent component.
+  @Input() comment!: CommentModel;
+
+  // State variable to control the visibility of the comment input field.
   showCommentInput = false;
+
+  // State variable to hold the text of the new comment being added.
   newCommentText = '';
-  comments: Comment[] = [];
+
+  // State variable to hold the value of the comment input field.
+  commentInputValue: string = '';
+
+  // Array to hold the list of comments.
+  comments: CommentModel[] = [];
+
+  // Predefined list of users for the tagging feature.
   users = [
     { userID: 1, name: 'Kevin' },
     { userID: 2, name: 'Jeff' },
     { userID: 3, name: 'Bryan' },
     { userID: 4, name: 'Gabbey' },
   ];
+
+  // Array to hold the filtered users based on the input after the "@" symbol.
   filteredUsers: any[] = [];
 
+  // Output event to notify the parent component when the comment icon is clicked.
   @Output() commentIconClicked = new EventEmitter<void>();
 
+  // Method to toggle the visibility of the comment input field.
   toggleCommentInput() {
     this.showCommentInput = !this.showCommentInput;
-    this.commentIconClicked.emit(); // Emit the event when the comment icon is clicked
+    this.commentIconClicked.emit();
   }
 
+  // Method to add a new comment to the comments array.
   addComment() {
     if (this.newCommentText.trim() !== '') {
-      const newComment: Comment = {
+      const newComment: CommentModel = {
         userName: 'User Name',
         text: this.newCommentText,
         timestamp: new Date(),
       };
       this.comments.push(newComment);
-      this.newCommentText = '';
+      this.commentInputValue = '';
     }
   }
-  // Variable to hold the current input value
-  commentInputValue: string = '';
 
-  // Method to detect '@' symbol
+  // Method to handle changes in the comment input field.
+  // It detects the "@" symbol and filters the users accordingly.
   onCommentInputChange(event: any) {
     this.commentInputValue = event.target.value;
-    this.newCommentText = this.commentInputValue; // Update the new comment text
+    this.newCommentText = this.commentInputValue;
 
     if (this.commentInputValue.includes('@')) {
-      // Logic to handle '@' symbol detection
       console.log('Detected @ symbol');
       const atIndex = this.commentInputValue.lastIndexOf('@');
       const afterAt =
@@ -55,17 +70,18 @@ export class CommentComponent {
         user.name.startsWith(afterAt)
       );
     } else {
-      this.filteredUsers = []; // Clear the filtered users if '@' is removed
+      this.filteredUsers = [];
     }
   }
 
+  // Method to select a user from the filtered users list and update the comment input value.
   selectUser(user: any) {
     const atIndex = this.commentInputValue.lastIndexOf('@');
     const beforeAt = this.commentInputValue.slice(0, atIndex);
     const afterAt = this.commentInputValue.slice(atIndex).split(' ')[1] || '';
 
     this.commentInputValue = `${beforeAt}@${user.name} ${afterAt}`;
-    this.newCommentText = this.commentInputValue; // Update the new comment text
-    this.filteredUsers = []; // Clear the filtered users
+    this.newCommentText = this.commentInputValue;
+    this.filteredUsers = [];
   }
 }
--- src/app/post/models/comment.model.ts
+++ src/app/post/models/comment.model.ts
@@ -1,4 +1,4 @@
-export interface Comment {
+export interface CommentModel {
   userName: string;
   text: string;
   timestamp: Date;
