503252f82c77339b83c84ec362d7bc82496aaa98
Dean Brittain <deanbrittain@Deans-MacBook-Pro.local>
2023-08-29 13:06:45 -0600

    revert to text area

--- src/app/post/components/comment/comment.component.ts
+++ src/app/post/components/comment/comment.component.ts
@@ -5,40 +5,59 @@ import {
   EventEmitter,
   ElementRef,
   ViewChild,
-  HostListener,
 } from '@angular/core';
+
 import { CommentModel } from '../../models/comment.model';
-import { ViewEncapsulation } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
   styleUrls: ['./comment.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class CommentComponent {
+  // Input property to receive the comment data from the parent component.
   @Input() comment!: CommentModel;
+
+  // State variable to control the visibility of the comment input field.
   showCommentInput = false;
+
+  // State variable to hold the text of the new comment being added.
   newCommentText = '';
+
+  // State variable to hold the value of the comment input field.
   commentInputValue: string = '';
+
+  // Array to hold the list of comments.
   comments: CommentModel[] = [];
+
   highlightedIndex: number = -1;
+
   lastSelectedUser: string = '';
+
+  // Predefined list of users for the tagging feature.
   users = [
     { userID: 1, name: 'Kevin' },
     { userID: 2, name: 'Jeff' },
     { userID: 3, name: 'Bryan' },
     { userID: 4, name: 'Gabbey' },
   ];
+
+  // Array to hold the filtered users based on the input after the "@" symbol.
   filteredUsers: any[] = [];
+
   @ViewChild('commentInput', { static: false }) commentInput!: ElementRef;
+
+  // Output event to notify the parent component when the comment icon is clicked.
   @Output() commentIconClicked = new EventEmitter<void>();
 
+  // Method to toggle the visibility of the comment input field.
   toggleCommentInput() {
     this.showCommentInput = !this.showCommentInput;
     this.commentIconClicked.emit();
   }
 
+  // Method to add a new comment to the comments array.
   addComment() {
     if (this.newCommentText.trim() !== '') {
       const newComment: CommentModel = {
@@ -51,97 +70,60 @@ export class CommentComponent {
     }
   }
 
+  // Method to handle changes in the comment input field.
+  // It detects the "@" symbol and filters the users accordingly.
   onCommentInputChange(event: any) {
-    this.commentInputValue = this.commentInput.nativeElement.innerText;
+    this.commentInputValue = event.target.value;
     this.newCommentText = this.commentInputValue;
 
-    let isTagging = false;
-
-    if (this.commentInputValue.includes('@')) {
-      const words = this.commentInputValue.split(' ');
-      const lastWord = words[words.length - 1];
-
-      if (lastWord.startsWith('@')) {
-        isTagging = true;
-        const afterAt = lastWord.slice(1);
-        const lowerCaseAfterAt = afterAt.toLowerCase();
-
-        this.filteredUsers = this.users.filter((user) =>
-          user.name.toLowerCase().includes(lowerCaseAfterAt)
-        );
-      }
+    // Reset lastSelectedUser if a new @ symbol is typed after the last selected user's name
+    const lastSelectedUserIndex = this.commentInputValue.lastIndexOf(
+      this.lastSelectedUser
+    );
+    if (
+      lastSelectedUserIndex !== -1 &&
+      this.commentInputValue.indexOf(
+        '@',
+        lastSelectedUserIndex + this.lastSelectedUser.length
+      ) !== -1
+    ) {
+      this.lastSelectedUser = '';
+    } else if (!this.commentInputValue.includes(this.lastSelectedUser)) {
+      this.lastSelectedUser = '';
     }
 
-    if (!isTagging) {
+    if (!this.commentInputValue.includes(this.lastSelectedUser)) {
+      this.lastSelectedUser = '';
+    }
+
+    if (this.commentInputValue.includes('@') && !this.lastSelectedUser) {
+      console.log('Detected @ symbol');
+      const atIndex = this.commentInputValue.lastIndexOf('@');
+      const afterAt =
+        this.commentInputValue.slice(atIndex + 1).split(' ')[0] || '';
+      this.filteredUsers = this.users.filter((user) =>
+        user.name.startsWith(afterAt)
+      );
+    } else {
       this.filteredUsers = [];
     }
-
     this.highlightedIndex = -1;
   }
 
+  // Method to select a user from the filtered users list and update the comment input value.
   selectUser(user: any) {
-    const atIndex = this.commentInput.nativeElement.innerHTML.lastIndexOf('@');
-    const beforeAt = this.commentInput.nativeElement.innerHTML.slice(
-      0,
-      atIndex
-    );
-    const afterAt =
-      this.commentInput.nativeElement.innerHTML
-        .slice(atIndex + 1)
-        .split(' ')[1] || '';
+    const atIndex = this.commentInputValue.lastIndexOf('@');
+    const beforeAt = this.commentInputValue.slice(0, atIndex);
+    const afterAt = this.commentInputValue.slice(atIndex).split(' ')[1] || '';
 
-    const wrappedName = user.name
-      .split('')
-      .map(
-        (char: string, index: number) =>
-          `<span style="animation-delay: ${index * 0.1}s">${char}</span>`
-      )
-      .join('');
-
-    // Create a new span element for the tagged name
-    const newTag = document.createElement('span');
-    newTag.className = 'tagged-name new-tag animate';
-
-    // Create a new span element for the highlighted tag
-    const highlightedTag = document.createElement('span');
-    highlightedTag.className = 'highlighted-tag';
-    highlightedTag.innerHTML = '@' + wrappedName;
-
-    // Append the highlighted tag to the new tag
-    newTag.appendChild(highlightedTag);
-
-    // Update the innerHTML and append the new tag
-    this.commentInput.nativeElement.innerHTML = `${beforeAt} ${afterAt}`;
-    this.commentInput.nativeElement.appendChild(newTag);
-
-    // Remove the 'new-tag' and 'animate' classes from all tags
-    this.commentInput.nativeElement
-      .querySelectorAll('.tagged-name')
-      .forEach((tag: HTMLElement) => {
-        tag.classList.remove('new-tag');
-        tag.classList.remove('animate');
-      });
-
-    // Add the 'new-tag' and 'animate' classes only to the newly added tag
-    newTag.classList.add('new-tag');
-    newTag.classList.add('animate');
-
-    // Update the commentInputValue and newCommentText
-    this.commentInputValue = this.commentInput.nativeElement.innerText;
+    this.commentInputValue = `${beforeAt}@${user.name} ${afterAt}`;
     this.newCommentText = this.commentInputValue;
 
-    // Hide the dropdown and reset the highlighted index
+    // This should hide the dropdown
     this.filteredUsers = [];
     this.highlightedIndex = -1;
-    this.lastSelectedUser = `@${user.name}`; // Keep the "@" symbol here
-
-    // Set the cursor at the end
-    const range = document.createRange();
-    const selection = window.getSelection();
-    range.selectNodeContents(this.commentInput.nativeElement);
-    range.collapse(false);
-    selection?.removeAllRanges();
-    selection?.addRange(range);
+    this.lastSelectedUser = `@${user.name}`;
+    this.commentInput.nativeElement.focus();
   }
 
   @HostListener('keydown', ['$event'])
