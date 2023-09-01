1503fc4fd2b9620d60fcae35cd6b2aeecafcbf56
Dean Brittain <deanbrittain@Deans-MacBook-Pro.local>
2023-08-31 20:38:08 -0600

    refactoring

--- src/app/post/components/comment/comment.component.ts
+++ src/app/post/components/comment/comment.component.ts
@@ -1,16 +1,20 @@
 import {
   Component,
-  Input,
   Output,
   EventEmitter,
   ElementRef,
-  ViewChild,
+  ViewChild, // What is this doing? Why is it needed?
   Renderer2,
-  HostListener,
 } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { CommentModel } from '../../models/comment.model';
+import { HostListener } from '@angular/core';
+
+interface User {
+  userID: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-comment',
@@ -20,24 +24,22 @@ import { CommentModel } from '../../models/comment.model';
 export class CommentComponent {
   constructor(private snackBar: MatSnackBar, private renderer: Renderer2) {}
 
-  @Input() comment!: CommentModel;
   @Output() commentIconClicked = new EventEmitter<void>();
 
   showCommentInput = false;
-  newCommentText = '';
-  commentInputValue: string = '';
+  newCommentText: string = '';
   comments: CommentModel[] = [];
-  highlightedIndex: number = -1;
-  lastSelectedUser: string = '';
+  highlightedIndex = -1;
+  lastSelectedUser = '';
 
-  users = [
+  users: User[] = [
     { userID: 1, name: 'Kevin' },
     { userID: 2, name: 'Jeff' },
     { userID: 3, name: 'Bryan' },
     { userID: 4, name: 'Gabbey' },
   ];
 
-  filteredUsers: any[] = [];
+  filteredUsers: User[] = [];
 
   @ViewChild('commentInput', { static: false }) commentInput!: ElementRef;
 
@@ -46,68 +48,112 @@ export class CommentComponent {
     this.commentIconClicked.emit();
   }
 
-  // Method to add a new comment to the comments array.
   addComment() {
-    if (this.newCommentText.trim() !== '') {
-      const newComment: CommentModel = {
-        userName: 'User Name',
-        text: this.newCommentText.trim(),
-        timestamp: new Date(),
-      };
-      this.comments.push(newComment);
-      this.commentInputValue = '';
-
-      // Extract tagged usernames
-      const taggedUsers = this.newCommentText.match(/@\w+/g);
-      if (taggedUsers) {
-        // Filter only valid users
-        const validUserNames = this.users.map((user) => user.name);
-        const filteredTaggedUsers = taggedUsers.filter((tag) =>
-          validUserNames.includes(tag.slice(1))
-        );
-
-        // Create toast message only if there are valid users
-        if (filteredTaggedUsers.length > 0) {
-          const userNames = filteredTaggedUsers.map((user) => user.slice(1)); // Remove '@'
-          let toastMessage = '';
-          if (userNames.length === 1) {
-            toastMessage = `${userNames[0]} has been notified`;
-          } else {
-            const lastUser = userNames.pop();
-            toastMessage = `${userNames.join(
-              ', '
-            )} and ${lastUser} have been notified`;
-          }
-          // Display the toast (replace this with your actual toast logic)
-          this.snackBar.open(toastMessage, 'Close', {
-            duration: 3000,
-          });
-        }
-      }
+    const trimmedText = this.newCommentText.trim();
+    if (trimmedText !== '') {
+      this.createNewComment(trimmedText);
+      this.notifyTaggedUsers(trimmedText);
+      // Reset the comment input field
+      this.commentInput.nativeElement.innerHTML = '';
     }
   }
 
-  onCommentInputChange(event: any) {
-    // Get the text content from the contenteditable element
-    let content = this.commentInput.nativeElement.textContent;
+  createNewComment(text: string) {
+    const newComment: CommentModel = {
+      userName: 'User Name',
+      text: text,
+      timestamp: new Date(),
+    };
+    this.comments.push(newComment);
+  }
 
-    // Update the component's state variables with the new content
-    this.commentInputValue = content;
-    this.newCommentText = content; //*** WHY ARE WE CAPTURING THIS TWICE? */
+  notifyTaggedUsers(text: string) {
+    const taggedUsers = this.extractTaggedUsers(text) as string[];
+    const validUserNames = this.filterValidUsers(taggedUsers);
 
-    // Use regex to find all instances of tagged names (e.g., @John)
-    const taggedNames = content.match(/@\w+/g) || [];
+    // Map valid usernames to their corresponding user IDs
+    const validUserIDs = validUserNames
+      .map((name) => {
+        const user = this.users.find((user) => user.name === name);
+        return user ? user.userID : null;
+      })
+      .filter(Boolean); // Remove any null values
 
-    // Initialize a variable to hold the styled content
+    // Remove duplicate user IDs
+    const uniqueValidUserIDs = [...new Set(validUserIDs)];
+
+    const uniqueValidUserNames = uniqueValidUserIDs
+      .map((id) => {
+        const user = this.users.find((user) => user.userID === id);
+        return user ? user.name : null;
+      })
+      .filter(Boolean) as string[]; // Type assertion here
+
+    if (uniqueValidUserNames.length > 0) {
+      const toastMessage = this.generateToastMessage(uniqueValidUserNames);
+      this.snackBar.open(toastMessage, 'Close', {
+        duration: 3000,
+      });
+    }
+  }
+
+  extractTaggedUsers(text: string): string[] {
+    return text.match(/@\w+/g) || [];
+  }
+
+  filterValidUsers(taggedUsers: string[]): string[] {
+    const validUserNames = this.users.map((user) => user.name);
+    return taggedUsers
+      .filter((tag) => validUserNames.includes(tag.slice(1)))
+      .map((user) => user.slice(1));
+  }
+
+  generateToastMessage(userNames: string[]): string {
+    let message = '';
+    if (userNames.length === 1) {
+      message = `${userNames[0]} has been notified`;
+    } else {
+      const lastUser = userNames.pop();
+      message = `${userNames.join(', ')} and ${lastUser} have been notified`;
+    }
+    return message;
+  }
+
+  onCommentInputChange(event: Event) {
+    const content = this.commentInput.nativeElement.textContent;
+    this.updateNewCommentText(content);
+    this.styleTaggedNames(content);
+    this.setCursorPosition();
+    this.detectUserTagging(content);
+    // You can now use the event object if needed
+    console.log(event); // For demonstration; you might want to remove this in production
+  }
+
+  updateNewCommentText(content: string) {
+    this.newCommentText = content;
+  }
+
+  styleTaggedNames(content: string) {
+    const taggedNames = this.extractTaggedNames(content);
+    const styledContent = this.applyStylesToTags(content, taggedNames);
+
+    const cursorPos = this.captureCursorPosition();
+
+    this.updateCommentInput(styledContent, taggedNames.length > 0);
+
+    this.restoreCursorPosition(cursorPos);
+  }
+
+  extractTaggedNames(content: string): string[] {
+    return content.match(/@\w+/g) || [];
+  }
+
+  applyStylesToTags(content: string, taggedNames: string[]): string {
     let styledContent = content;
-
     const validUsers = this.users.map((user) => user.name);
 
-    taggedNames.forEach((tag: string) => {
-      // Remove the '@' symbol from the tag for checking
+    taggedNames.forEach((tag) => {
       const tagName = tag.replace('@', '');
-
-      // Check if the tag is a valid user
       if (validUsers.includes(tagName)) {
         styledContent = styledContent.replace(
           new RegExp(tag, 'g'),
@@ -116,77 +162,134 @@ export class CommentComponent {
       }
     });
 
-    // Update the innerHTML only if there are tagged names
-    if (taggedNames.length > 0) {
-      this.commentInput.nativeElement.innerHTML = styledContent; // WHAT IS THIS LINE DOING EXACTLY?
+    return styledContent;
+  }
+
+  captureCursorPosition(): number {
+    const sel = window.getSelection();
+    let cursorPos = 0;
+
+    if (sel && sel.rangeCount) {
+      const range = sel.getRangeAt(0);
+      cursorPos = range.startOffset;
     }
 
-    // Create a new range object to manipulate the cursor position
-    const range = document.createRange();
+    return cursorPos;
+  }
 
-    // Get the current selection object
+  updateCommentInput(styledContent: string, hasTags: boolean) {
+    if (hasTags) {
+      this.commentInput.nativeElement.innerHTML = styledContent;
+    }
+  }
+
+  restoreCursorPosition(cursorPos: number) {
     const sel = window.getSelection();
 
-    // Check if a selection object exists
     if (sel) {
-      // Set the cursor position to the end of the content
+      const firstChild = this.commentInput.nativeElement.firstChild;
+
+      if (firstChild && firstChild.nodeType === Node.TEXT_NODE) {
+        let range = document.createRange();
+        const maxCursorPos = firstChild.textContent
+          ? firstChild.textContent.length
+          : 0;
+        range.setStart(firstChild, Math.min(cursorPos, maxCursorPos));
+        range.collapse(true);
+
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
+    }
+  }
+
+  setCursorPosition() {
+    const range = document.createRange();
+    const sel = window.getSelection();
+    if (sel) {
       range.selectNodeContents(this.commentInput.nativeElement);
       range.collapse(false);
       sel.removeAllRanges();
       sel.addRange(range);
     }
+  }
 
-    // Check if the last selected user is still in the content
+  detectUserTagging(content: string) {
+    this.resetLastSelectedUserIfNecessary(content);
+
+    if (this.isTaggingMode(content)) {
+      this.filterUsersBasedOnTag(content);
+    } else {
+      this.filteredUsers = [];
+    }
+    this.highlightedIndex = -1;
+  }
+
+  resetLastSelectedUserIfNecessary(content: string) {
     const lastSelectedUserIndex = content.lastIndexOf(this.lastSelectedUser);
-    if (
-      lastSelectedUserIndex !== -1 &&
+    const isUserAfterAtSymbol =
       content.indexOf(
         '@',
         lastSelectedUserIndex + this.lastSelectedUser.length
-      ) !== -1
+      ) !== -1;
+
+    if (
+      (lastSelectedUserIndex !== -1 && isUserAfterAtSymbol) ||
+      !content.includes(this.lastSelectedUser)
     ) {
-      // Reset the last selected user if a new '@' is typed
-      this.lastSelectedUser = '';
-    } else if (!content.includes(this.lastSelectedUser)) {
-      // Reset the last selected user if it's no longer in the content
       this.lastSelectedUser = '';
     }
+  }
 
-    // Detect if a new '@' symbol is typed
-    if (content.includes('@') && !this.lastSelectedUser) {
-      console.log('Detected @ symbol');
-      const atIndex = content.lastIndexOf('@');
-      const afterAt = content.slice(atIndex + 1).split(' ')[0] || '';
+  isTaggingMode(content: string): boolean {
+    return content.includes('@') && !this.lastSelectedUser;
+  }
 
-      // Filter the list of users based on the text after the '@'
-      this.filteredUsers = this.users.filter((user) =>
-        user.name.startsWith(afterAt)
-      );
-    } else {
-      // Reset the filtered users list if no '@' is detected
-      this.filteredUsers = [];
-    }
-
-    // Reset the highlighted index for the dropdown
-    this.highlightedIndex = -1;
+  filterUsersBasedOnTag(content: string) {
+    const atIndex = content.lastIndexOf('@');
+    const afterAt = content.slice(atIndex + 1).split(' ')[0] || '';
+    this.filteredUsers = this.users.filter((user) =>
+      user.name.startsWith(afterAt)
+    );
   }
 
   // Method to select a user from the filtered users list and update the comment input value.
   selectUser(user: any) {
-    let content = this.commentInput.nativeElement.textContent; // Use textContent
-    content = content.trim(); // Explicitly trim the text
+    let content = this.getCommentInputText();
+    const newContent = this.buildNewContent(content, user);
+
+    this.updateCommentInputAndState(newContent);
+
+    this.addSpaceAfterContent();
+
+    this.resetUserDropdown();
+  }
+
+  // Extracts the text content from the comment input
+  getCommentInputText(): string {
+    return this.commentInput.nativeElement.textContent.trim();
+  }
+
+  // Builds the new comment content based on the selected user
+  buildNewContent(content: string, user: any): string {
     const atIndex = content.lastIndexOf('@');
     const beforeAt = content.slice(0, atIndex);
     const afterAt = content.slice(atIndex).split(' ')[1] || '';
 
-    const newContent = `${beforeAt}@${user.name}${afterAt}`;
-    this.commentInput.nativeElement.innerHTML = newContent.trim(); // Trim here too
-    this.newCommentText = newContent.trim(); // Trim here too
+    return `${beforeAt}@${user.name}${afterAt}`.trim();
+  }
 
+  // Updates the inner HTML of the comment input and component state
+  updateCommentInputAndState(newContent: string) {
+    this.commentInput.nativeElement.innerHTML = newContent;
+    this.newCommentText = newContent;
+  }
+
+  // Adds a space after the newly inserted username and updates the cursor position
+  addSpaceAfterContent() {
     const textNode = this.renderer.createText(' '); // This is a space
     this.renderer.appendChild(this.commentInput.nativeElement, textNode);
 
-    // Update the cursor position to be after the new text node
     const range = document.createRange();
     const sel = window.getSelection();
     if (sel) {
@@ -195,33 +298,54 @@ export class CommentComponent {
       sel.removeAllRanges();
       sel.addRange(range);
     }
+  }
 
-    // This should hide the dropdown
+  // Resets the user dropdown and other related states
+  resetUserDropdown() {
     this.filteredUsers = [];
     this.highlightedIndex = -1;
-    this.lastSelectedUser = `@${user.name}`;
+    this.lastSelectedUser = this.newCommentText;
     this.commentInput.nativeElement.focus();
   }
 
   @HostListener('keydown', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (this.filteredUsers.length > 0) {
-      if (event.key === 'ArrowDown') {
-        if (this.highlightedIndex < this.filteredUsers.length - 1) {
-          this.highlightedIndex++;
-        }
-      }
-      if (event.key === 'ArrowUp') {
-        if (this.highlightedIndex > 0) {
-          this.highlightedIndex--;
-        }
-      }
-      if (event.key === 'Enter') {
-        if (this.highlightedIndex >= 0) {
-          event.preventDefault();
-          this.selectUser(this.filteredUsers[this.highlightedIndex]);
-        }
-      }
+    if (this.filteredUsers.length === 0) {
+      return;
+    }
+
+    switch (event.key) {
+      case 'ArrowDown':
+        this.moveHighlightDown();
+        break;
+      case 'ArrowUp':
+        this.moveHighlightUp();
+        break;
+      case 'Enter':
+        this.selectHighlightedUser(event);
+        break;
+    }
+  }
+
+  // Move highlighted index down
+  moveHighlightDown() {
+    if (this.highlightedIndex < this.filteredUsers.length - 1) {
+      this.highlightedIndex++;
+    }
+  }
+
+  // Move highlighted index up
+  moveHighlightUp() {
+    if (this.highlightedIndex > 0) {
+      this.highlightedIndex--;
+    }
+  }
+
+  // Select the highlighted user
+  selectHighlightedUser(event: KeyboardEvent) {
+    if (this.highlightedIndex >= 0) {
+      event.preventDefault();
+      this.selectUser(this.filteredUsers[this.highlightedIndex]);
     }
   }
 }
